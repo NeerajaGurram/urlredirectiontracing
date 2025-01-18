100672bc300b706509ddd6e80fcb5c1ba794c736
Neerajagurram <152693538+NeerajaGurram@users.noreply.github.com>
2025-01-18 23:56:47 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.{html}"  
+    "./public/**/*.html"  
   ],
   theme: {
     extend: {
